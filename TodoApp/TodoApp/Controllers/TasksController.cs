57f0164fc2203ada23247be9debd9e00e1eb4ecd
ST10294145 <128233215+ST10294145@users.noreply.github.com>
2024-03-21 00:14:41 +0200

    Add files via upload

---
+++ TodoApp/TodoApp/Controllers/TasksController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly TaskRepository _taskRepository;
+
+        public TasksController(TaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var tasks = _taskRepository.GetAllTasks();
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Task task)
+        {
+            _taskRepository.AddTask(task);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var task = _taskRepository.GetTaskById(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            _taskRepository.UpdateTask(task);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var task = _taskRepository.GetTaskById(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _taskRepository.DeleteTask(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TodoApp/TodoApp/TaskRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TodoApp.Models;
+
+namespace TodoApp.Models
+{
+    public class TaskRepository
+    {
+        private static List<Task> _tasks = new List<Task>
+        {
+            new Task { Id = 1, Title = "Task 1", Description = "Description for Task 1" },
+            new Task { Id = 2, Title = "Task 2", Description = "Description for Task 2" },
+            new Task { Id = 3, Title = "Task 3", Description = "Description for Task 3" }
+        };
+
+        public IEnumerable<Task> GetAllTasks()
+        {
+            return _tasks;
+        }
+
+        public Task GetTaskById(int id)
+        {
+            return _tasks.FirstOrDefault(t => t.Id == id);
+        }
+
+        public void AddTask(Task task)
+        {
+            task.Id = _tasks.Count + 1;
+            _tasks.Add(task);
+        }
+
+        public void UpdateTask(Task updatedTask)
+        {
+            var existingTask = _tasks.FirstOrDefault(t => t.Id == updatedTask.Id);
+            if (existingTask != null)
+            {
+                existingTask.Title = updatedTask.Title;
+                existingTask.Description = updatedTask.Description;
+            }
+        }
+
+        public void DeleteTask(int id)
+        {
+            _tasks.RemoveAll(t => t.Id == id);
+        }
+    }
+}
